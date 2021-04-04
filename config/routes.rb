931782fe095924f6cbf97672aced908bfa46c3d0
Jean Battirola <jeanbattirola@gmail.com>
2021-04-04 21:40:27 +0200

    routes, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  root to: 'companies#index' 
+  resources :companies, only: %i[ index show ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
